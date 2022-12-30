049dfebda2c99a310c173475d22f3c9174845d11
git Pico <saxiesluar7@gmail.com>
2022-12-30 07:18:38 -0600

    modifiacion, traduccion de returns, tags, uasers

--- resources/js/components/passport/Clients.vue
+++ resources/js/components/passport/Clients.vue
@@ -1,7 +1,7 @@
 <style scoped>
-    .action-link {
-        cursor: pointer;
-    }
+.action-link {
+    cursor: pointer;
+}
 </style>
 
 <template>
@@ -29,7 +29,7 @@
                     <thead>
                         <tr>
                             <th>Client ID</th>
-                            <th>Name</th>
+                            <th>Nombre</th>
                             <th>Secret</th>
                             <th></th>
                             <th></th>
@@ -103,8 +103,8 @@
                                 <label class="col-md-3 col-form-label">Name</label>
 
                                 <div class="col-md-9">
-                                    <input id="create-client-name" type="text" class="form-control"
-                                                                @keyup.enter="store" v-model="createForm.name">
+                                    <input id="create-client-name" type="text" class="form-control" @keyup.enter="store"
+                                        v-model="createForm.name">
 
                                     <span class="form-text text-muted">
                                         Something your users will recognize and trust.
@@ -117,8 +117,8 @@
                                 <label class="col-md-3 col-form-label">Redirect URL</label>
 
                                 <div class="col-md-9">
-                                    <input type="text" class="form-control" name="redirect"
-                                                    @keyup.enter="store" v-model="createForm.redirect">
+                                    <input type="text" class="form-control" name="redirect" @keyup.enter="store"
+                                        v-model="createForm.redirect">
 
                                     <span class="form-text text-muted">
                                         Your application's authorization callback URL.
@@ -138,7 +138,10 @@
                                     </div>
 
                                     <span class="form-text text-muted">
-                                        Require the client to authenticate with a secret. Confidential clients can hold credentials in a secure way without exposing them to unauthorized parties. Public applications, such as native desktop or JavaScript SPA applications, are unable to hold secrets securely.
+                                        Require the client to authenticate with a secret. Confidential clients can hold
+                                        credentials in a secure way without exposing them to unauthorized parties.
+                                        Public applications, such as native desktop or JavaScript SPA applications, are
+                                        unable to hold secrets securely.
                                     </span>
                                 </div>
                             </div>
@@ -188,8 +191,8 @@
                                 <label class="col-md-3 col-form-label">Name</label>
 
                                 <div class="col-md-9">
-                                    <input id="edit-client-name" type="text" class="form-control"
-                                                                @keyup.enter="update" v-model="editForm.name">
+                                    <input id="edit-client-name" type="text" class="form-control" @keyup.enter="update"
+                                        v-model="editForm.name">
 
                                     <span class="form-text text-muted">
                                         Something your users will recognize and trust.
@@ -202,8 +205,8 @@
                                 <label class="col-md-3 col-form-label">Redirect URL</label>
 
                                 <div class="col-md-9">
-                                    <input type="text" class="form-control" name="redirect"
-                                                    @keyup.enter="update" v-model="editForm.redirect">
+                                    <input type="text" class="form-control" name="redirect" @keyup.enter="update"
+                                        v-model="editForm.redirect">
 
                                     <span class="form-text text-muted">
                                         Your application's authorization callback URL.
@@ -228,141 +231,141 @@
 </template>
 
 <script>
-    export default {
-        /*
-         * The component's data.
-         */
-        data() {
-            return {
-                clients: [],
+export default {
+    /*
+     * The component's data.
+     */
+    data() {
+        return {
+            clients: [],
 
-                createForm: {
-                    errors: [],
-                    name: '',
-                    redirect: '',
-                    confidential: true
-                },
-
-                editForm: {
-                    errors: [],
-                    name: '',
-                    redirect: ''
-                }
-            };
-        },
-
-        /**
-         * Prepare the component (Vue 1.x).
-         */
-        ready() {
-            this.prepareComponent();
-        },
-
-        /**
-         * Prepare the component (Vue 2.x).
-         */
-        mounted() {
-            this.prepareComponent();
-        },
-
-        methods: {
-            /**
-             * Prepare the component.
-             */
-            prepareComponent() {
-                this.getClients();
-
-                $('#modal-create-client').on('shown.bs.modal', () => {
-                    $('#create-client-name').focus();
-                });
-
-                $('#modal-edit-client').on('shown.bs.modal', () => {
-                    $('#edit-client-name').focus();
-                });
+            createForm: {
+                errors: [],
+                name: '',
+                redirect: '',
+                confidential: true
             },
 
-            /**
-             * Get all of the OAuth clients for the user.
-             */
-            getClients() {
-                axios.get('/oauth/clients')
-                        .then(response => {
-                            this.clients = response.data;
-                        });
-            },
-
-            /**
-             * Show the form for creating new clients.
-             */
-            showCreateClientForm() {
-                $('#modal-create-client').modal('show');
-            },
-
-            /**
-             * Create a new OAuth client for the user.
-             */
-            store() {
-                this.persistClient(
-                    'post', '/oauth/clients',
-                    this.createForm, '#modal-create-client'
-                );
-            },
-
-            /**
-             * Edit the given client.
-             */
-            edit(client) {
-                this.editForm.id = client.id;
-                this.editForm.name = client.name;
-                this.editForm.redirect = client.redirect;
-
-                $('#modal-edit-client').modal('show');
-            },
-
-            /**
-             * Update the client being edited.
-             */
-            update() {
-                this.persistClient(
-                    'put', '/oauth/clients/' + this.editForm.id,
-                    this.editForm, '#modal-edit-client'
-                );
-            },
-
-            /**
-             * Persist the client to storage using the given form.
-             */
-            persistClient(method, uri, form, modal) {
-                form.errors = [];
-
-                axios[method](uri, form)
-                    .then(response => {
-                        this.getClients();
-
-                        form.name = '';
-                        form.redirect = '';
-                        form.errors = [];
-
-                        $(modal).modal('hide');
-                    })
-                    .catch(error => {
-                        if (typeof error.response.data === 'object') {
-                            form.errors = _.flatten(_.toArray(error.response.data.errors));
-                        } else {
-                            form.errors = ['Something went wrong. Please try again.'];
-                        }
-                    });
-            },
-
-            /**
-             * Destroy the given client.
-             */
-            destroy(client) {
-                axios.delete('/oauth/clients/' + client.id)
-                        .then(response => {
-                            this.getClients();
-                        });
+            editForm: {
+                errors: [],
+                name: '',
+                redirect: ''
             }
+        };
+    },
+
+    /**
+     * Prepare the component (Vue 1.x).
+     */
+    ready() {
+        this.prepareComponent();
+    },
+
+    /**
+     * Prepare the component (Vue 2.x).
+     */
+    mounted() {
+        this.prepareComponent();
+    },
+
+    methods: {
+        /**
+         * Prepare the component.
+         */
+        prepareComponent() {
+            this.getClients();
+
+            $('#modal-create-client').on('shown.bs.modal', () => {
+                $('#create-client-name').focus();
+            });
+
+            $('#modal-edit-client').on('shown.bs.modal', () => {
+                $('#edit-client-name').focus();
+            });
+        },
+
+        /**
+         * Get all of the OAuth clients for the user.
+         */
+        getClients() {
+            axios.get('/oauth/clients')
+                .then(response => {
+                    this.clients = response.data;
+                });
+        },
+
+        /**
+         * Show the form for creating new clients.
+         */
+        showCreateClientForm() {
+            $('#modal-create-client').modal('show');
+        },
+
+        /**
+         * Create a new OAuth client for the user.
+         */
+        store() {
+            this.persistClient(
+                'post', '/oauth/clients',
+                this.createForm, '#modal-create-client'
+            );
+        },
+
+        /**
+         * Edit the given client.
+         */
+        edit(client) {
+            this.editForm.id = client.id;
+            this.editForm.name = client.name;
+            this.editForm.redirect = client.redirect;
+
+            $('#modal-edit-client').modal('show');
+        },
+
+        /**
+         * Update the client being edited.
+         */
+        update() {
+            this.persistClient(
+                'put', '/oauth/clients/' + this.editForm.id,
+                this.editForm, '#modal-edit-client'
+            );
+        },
+
+        /**
+         * Persist the client to storage using the given form.
+         */
+        persistClient(method, uri, form, modal) {
+            form.errors = [];
+
+            axios[method](uri, form)
+                .then(response => {
+                    this.getClients();
+
+                    form.name = '';
+                    form.redirect = '';
+                    form.errors = [];
+
+                    $(modal).modal('hide');
+                })
+                .catch(error => {
+                    if (typeof error.response.data === 'object') {
+                        form.errors = _.flatten(_.toArray(error.response.data.errors));
+                    } else {
+                        form.errors = ['Something went wrong. Please try again.'];
+                    }
+                });
+        },
+
+        /**
+         * Destroy the given client.
+         */
+        destroy(client) {
+            axios.delete('/oauth/clients/' + client.id)
+                .then(response => {
+                    this.getClients();
+                });
         }
     }
+}
 </script>
